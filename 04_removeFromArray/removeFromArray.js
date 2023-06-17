0a431357dedc49fc17338405ce418d705d008ab5
aaronjudell <ajudell@gmail.com>
2023-06-17 18:58:19 -0400

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removeArgs) {
+    const newArray = [];
+    array.forEach((element) => {
+        if (!removeArgs.includes(element)) {
+            newArray.push(element);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
