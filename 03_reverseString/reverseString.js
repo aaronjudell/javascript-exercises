93db0d8dc571df6713eb9f2b8499017f1ec8b95b
aaronjudell <ajudell@gmail.com>
2023-06-17 18:22:10 -0400

    Complete Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(forward) {
+    let reverse = forward.split('');
+    reverse = reverse.reverse();
+    let result = reverse.join('');
+    return result;
 };
 
 // Do not edit below this line
