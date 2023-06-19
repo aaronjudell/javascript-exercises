d52b6fc845f901d6e62d265ac465dc5b7ebb846e
aaronjudell <ajudell@gmail.com>
2023-06-19 18:46:39 -0400

    Complete Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    let newPhrase = phrase.replace(/[,.!?\s]/g, "");
+    newPhrase = newPhrase.toLowerCase();
+    let i = 0;
+    let j = newPhrase.length - 1;
+    while (i < j) {
+        if (newPhrase[i] === newPhrase[j]) {
+            i++, j--;
+            continue;
+        }
+        else {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
