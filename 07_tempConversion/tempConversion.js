5cd897772b86c0d2c832768629c4254dbb2d44b7
aaronjudell <ajudell@gmail.com>
2023-06-19 13:18:06 -0400

    Complete Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  let cel = ((far - 32) * (5 / 9));
+  cel = +cel.toFixed(1);
+  return cel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let far = ((cel * (9 / 5) + 32));
+  far = +far.toFixed(1);
+  return far;
 };
 
 // Do not edit below this line
