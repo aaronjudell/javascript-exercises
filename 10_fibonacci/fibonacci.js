86b5a7bd75e0548e7e8d999898e71f7df1f2d6f1
aaronjudell <ajudell@gmail.com>
2023-06-19 19:09:16 -0400

    Complete Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = +num;
+    if (num < 1) {
+        return "OOPS";
+    }
+    num = num - 1; //Works better with the array.
+    fibArray = [1, 1];
+    let i = 1;
+    while (i < num) {
+        fibArray.push(fibArray[i] + fibArray[i-1]);
+        i++;
+    }
+    return fibArray[num];
 };
 
 // Do not edit below this line
