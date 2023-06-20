58d08960c2db7025752676868fbefc8b27cc155b
aaronjudell <ajudell@gmail.com>
2023-06-20 12:26:19 -0400

    Complete Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let oldestIndex = 0;
+    let oldestAge = 0;
+    const currentYear = new Date().getFullYear();
+    for (let i = 0; i < array.length; i++) {
+        let age = 0;
+        if (!('yearOfDeath' in array[i])) {
+            age = (currentYear - array[i].yearOfBirth);
+        }
+        else {
+            age = (array[i].yearOfDeath - array[i].yearOfBirth);
+        }
+        if (age > oldestAge) {
+            oldestIndex = i;
+            oldestAge = age;
+        }
+    }
+    return array[oldestIndex];
 };
 
 // Do not edit below this line
